f583fa462d678dcf6900d4f431e717a1ddf46e4a
Chris LeBombard <clebombard@my.wctc.edu>
2019-02-28 15:44:20 -0600

    Linked  to database and populated database.

---
+++ Northwind/Models/INorthwindRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using Northwind.Models;
+
+namespace Northwind.Models
+{
+    public interface INorthwindRepository
+    {
+        IQueryable<Category> Categories { get; }
+        IQueryable<Product> Products { get; }
+
+        // TODO: AddBlog, UpdateBlog, DeleteBlog
+        // TODO: AddPost, UpdatePost, DeletePost
+    }
+}
