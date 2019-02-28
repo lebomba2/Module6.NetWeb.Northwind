cfdc57e165abf084d905d6c0fff41de34b8802e4
BIT student <bitstudent@bitnet.wctc.edu>
2019-02-28 10:57:01 -0600

    Add project files.

---
+++ Northwind/Controller/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Northwind.Core.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
