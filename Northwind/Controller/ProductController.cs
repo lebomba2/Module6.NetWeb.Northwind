36d83a6f4f605ada0f78c36e940054abea7b2f74
lebomba2 <clebombard@my.wctc.edu>
2019-03-14 11:32:34 -0500

    Created ProductController Added Category Method Added Category Detail Method Updated the Home/Index View Created Category View Created the CategoryDetail View imported css and js

---
+++ Northwind/Controller/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Northwind.Models;
+
+namespace Northwind.Core.Controllers
+{
+    public class ProductController : Controller
+    {
+        // this controller depends on the BloggingRepository
+        private INorthwindRepository repository;
+        public ProductController(INorthwindRepository repo) => repository = repo;
+
+        // Category method passing the Category table, sorted by the CategoryName, to the Category View
+        public IActionResult Category() => View(repository.Categories.OrderBy(c => c.CategoryName));
+        // CategoryDetail method passing the Products table, filtered by CategoryId and Discontinued, ordered by ProductName
+        public IActionResult CategoryDetail(int id) => View(repository.Products.Where(p => p.CategoryId == id && p.Discontinued == false).OrderBy(p => p.ProductName));
+    }
+}
